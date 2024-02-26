b2dac3accbe071d85da45512f44bc2366f9e7cb1
안동연 <a56634368@gmail.com>
2024-02-26 12:03:19 +0900

    jsx

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import Hello from './Hello';
+import './App.css';
 
 function App() {
+  const wave = 'nice body';
+  const darkStyle = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: 24,
+    padding: '1rem'
+  }
+
   return (
-    <div>
+    <>
+      
+      <Hello // 열리는 태그 안에서는 주석을 적을 수 있다 
+      />
       <Hello/>
       <Hello/>
-      <Hello/>
-    </div>
+      <div style = {darkStyle}>안녕하지 못해요</div>
+      <div>{wave}</div>
+      <div className="gray-box">회색 상자</div>
+      {/*주석은 중괄호를 포함할 것*/}
+    </>
   );
 }
 
